ca1f0ba4e9bcd6e7e4e1539402deb3d2f19d307e
Yazan Aqel <yazan.aqel93@gmail.com>
2024-01-05 12:05:12 +0300

    Add project files.

---
+++ Chat_SignalR_BlazorWASM/Chat_SignalR_BlazorWASM/ChatHubs/ChatHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace Chat_SignalR_BlazorWASM.ChatHubs;
+
+public class ChatHub : Hub
+{
+	public async Task SendMessage(string user, string message)
+	{
+		await Clients.All.SendAsync("ReceiveMessage", user, message);
+	}
+}
